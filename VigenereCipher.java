91ab90aba6216965731ce526cb00189df3e19373
Colin Smith <smithco@arch07.cis.gvsu.edu>
2018-03-28 10:43:44 -0400

    added the text visualization of the board as well as more helpful feedback

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,10 +1,48 @@
+import java.util.Arrays;
+
+
 public class VigenereCipher {
     public static void main(String[] args) {
-        String key = "VIGENERECIPHER";
-        String ori = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
-        String enc = encrypt(ori, key);
-        System.out.println(enc);
-        System.out.println(decrypt(enc, key));
+      String key = "VIGENERECIPHER";
+      String ori = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";
+      System.out.println("String: " + ori + "\nKey: " + key);
+
+      VigenereCipher vig = new VigenereCipher();
+      char[][] board = new char[26][26];
+      vig.generateBoard(board);
+
+      String enc = encrypt(ori, key);
+      System.out.println("Encrypted Message: " + enc);
+      System.out.println("Decrypted message: " + decrypt(enc, key));
+    }
+
+    /***************************************************************************
+    * generateBoard
+    * Generates a text based board which will be used as a stepping stone
+    * for our gui based board later.
+    * @param encBoard is the 2d char array used to visualize the V Cipher
+    ***************************************************************************/
+    public void generateBoard(char[][] encBoard){
+      int disp = 26, intToChar = 0;
+      char spot;
+      for(int i = 25; i>-1; i--){
+        for(int j = 0; j<26; j++){
+          intToChar = j + 65 + disp;
+          if(intToChar >= 91){
+            intToChar = intToChar - 26;
+          }
+          spot = (char)(intToChar);
+          encBoard[i][j] = spot;
+        }
+        disp--;
+      }
+
+      for(int i=0;i<26;i++){
+        for(int j=0;j<26;j++){
+          System.out.print(encBoard[i][j] + " ");
+        }
+        System.out.println();
+      }
     }
 
     static String encrypt(String text, final String key) {
